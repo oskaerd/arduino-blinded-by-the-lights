2dd93946c8fe6c372fa25ba4589adf58a3c94c79
Oskar Baginski <oskbag@gmail.com>
2021-09-05 01:32:51 +0200

    dirty and bad commit but callback works as expected

--- host/audiostream/audiostream.py
+++ host/audiostream/audiostream.py
@@ -5,18 +5,31 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
-CHUNK = 1024
+CHUNK = 2048
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 RAW_DATA = False
 
 
+def tmp_wav_fft(filename):
+    from scipy.io import wavfile as wav
+    from scipy.fftpack import fft
+    rate, data = wav.read(filename)
+    fft_out = fft(data)
+    plt.plot(data, np.abs(fft_out))
+    plt.show()
+
+
 #todo implement switching of the device
 class AudioStream:
-    def __init__(self, format=FORMAT, channels=CHANNELS, rate=RATE, chunk=CHUNK):
+    def __init__(self, channels=CHANNELS, rate=RATE, chunk=CHUNK, non_blocking=True):
         self.audio = pyaudio.PyAudio()
+        self.then = datetime.now()
         self.frames = []
+        self.plot_data = []
+        self.plot_left_channel = []
+        self.plot_right_channel = []
         self.streaming = False
 
         # search for loopback device
@@ -28,12 +41,17 @@ class AudioStream:
                 self.input_dev_idx = d['index']
                 break
 
-        self.stream = self.audio.open(format=format,
+        self.stream = self.audio.open(format=pyaudio.paInt16,
                                       channels=channels,
                                       rate=rate,
                                       input=True,
                                       input_device_index=self.input_dev_idx,
                                       frames_per_buffer=chunk)
+                                      # stream_callback=self.stream_callback)
+
+    def __del__(self):
+        print('* closing stream')
+        self.stream.close()
 
     def list_devices(self, key=None, value=None):
         devices = [self.audio.get_device_info_by_index(i) for i in range(self.audio.get_device_count())]
@@ -46,11 +64,29 @@ class AudioStream:
 
     # TODO: add the one with callback
     # todo implement actual streaming start/stop and remove seconds
-    def start_stream(self, seconds):
-        pass
+    def start_stream(self):
+        print('* starting stream')
+        self.streaming = True
+        self.then = datetime.now()
+        self.stream.start_stream()
 
     def stop_stream(self):
         self.streaming = False
+        now = datetime.now()
+        print(f'end: {now.second * 1000000 + now.microsecond - (self.then.second * 1000000 + self.then.microsecond)}')
+
+    def stream_callback(self, in_data, frame_count, time_info, status):
+        #now = datetime.now()
+        #print(f'cb: {now.second * 1000000 + now.microsecond - (self.then.second * 1000000 + self.then.microsecond)}')
+        #self.then = datetime.now()
+        self.plot_data = self.plot_data + list(np.frombuffer(in_data, np.int16))
+
+        if len(self.plot_data) >= 2 * 3 * 44100:
+            print('complete')
+            self.streaming = False
+            return in_data, pyaudio.paComplete
+        else:
+            return in_data, pyaudio.paContinue
 
     def record(self, seconds, output_filename):
         if seconds <= 0:
@@ -59,31 +95,33 @@ class AudioStream:
         print("* recording")
         self.frames = []
 
-        for i in range(0, int(RATE / CHUNK * seconds)):
-            data = self.stream.read(CHUNK)
-            self.frames.append(data)
+        for s in range(1):
+            self.frames = []
+            for i in range(0, int(RATE / CHUNK * 3)):
+                data = self.stream.read(CHUNK)
+                self.frames.append(data)
 
-        print('* done recording')
+            print('* done recording')
+
+            # todo make it working with raw data instead of this scuffed POS
+            # time_series, sample_rate = librosa.load(output_filename)
+            # stft = np.abs(librosa.stft(time_series, hop_length=512, n_fft=2048))
+
+            if RAW_DATA:
+                with open('dump.raw', 'w') as rawfile:
+                    rawdata = self.frames
+                    rawdata = [str(r) + '\n' for r in rawdata]
+                    rawfile.writelines(rawdata)
+
+            then = datetime.now()
+            wf = wave.open(f'{output_filename[:-4]}_{s}.wav', 'wb')
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(self.audio.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b''.join(self.frames))
+            #tmp_wav_fft(f'{output_filename[:-4]}_{s}.wav')
+            wf.close()
+            now = datetime.now()
+            print(now.second-then.second)
+            print(now.microsecond-then.microsecond)
         self.stream.stop_stream()
-
-        # todo make librosa working with raw data instead of this scuffed POS
-        # time_series, sample_rate = librosa.load(output_filename)
-        # stft = np.abs(librosa.stft(time_series, hop_length=512, n_fft=2048))
-
-        if RAW_DATA:
-            with open('dump.raw', 'w') as rawfile:
-                rawdata = self.frames
-                rawdata = [str(r) + '\n' for r in rawdata]
-                rawfile.writelines(rawdata)
-
-        then = datetime.now()
-        wf = wave.open(output_filename, 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(self.audio.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(self.frames))
-        wf.close()
-        now = datetime.now()
-        print(now.second-then.second)
-        print(now.microsecond-then.microsecond)
-
--- host/main.py
+++ host/main.py
@@ -5,14 +5,42 @@ from arduino_comms.arduino_comms import LedDriver, list_devices
 #todo integrate pyaudio fork to work in loopback mode
 #todo cleanup on exit
 
-RECORD_SECONDS = 210
+RECORD_SECONDS = 10
 WAVE_OUTPUT_FILENAME = 'tmp.wav'
 
-#stream = AudioStream()
-#stream.record(RECORD_SECONDS, WAVE_OUTPUT_FILENAME)
+stream = AudioStream()
+stream.record(3, WAVE_OUTPUT_FILENAME)
+# stream.start_stream()
+#
+# while stream.streaming:
+#     pass
+#
+# stream.stop_stream()
+#
+# left = []
+# right = []
+# cnt = 0
+# for i in stream.plot_data:
+#     if cnt % 2 == 0:
+#         left.append(i)
+#     else:
+#         right.append(i)
+#     cnt+=1
+#
+# import numpy as np
+# import matplotlib.pyplot as plt
+# x = np.linspace(0, len(left)/44100, num=int(len(left)))
+#
+# fig, ax = plt.subplots(2)
+# ax[0].clear()
+# ax[1].clear()
+# ax[0].plot(x, left)
+# ax[1].plot(x, right)
+# plt.subplots_adjust(bottom=0.20)
+# plt.show()
 
-com = list_devices()[0].name.split(' ')[0]
-led = LedDriver(com)
+# com = list_devices()[0].name.split(' ')[0]
+# led = LedDriver(com)
 
 # while True:
 #     # todo remove random
